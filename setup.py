fd9f32a7a37f29cf28946dc79b6fdb94f103c806
Pomilon <220483426+Pomilon@users.noreply.github.com>
2026-08-29 13:34:20 +0100

    fix: allow unsupported g++ for CUDA 12.8 and trim sm60 gencode for T4 build

--- setup.py
+++ setup.py
@@ -11,9 +11,7 @@ try:
             sources=['src/lema/_csrc/memory_manager.cpp', 'src/lema/_csrc/w8a8_cuda.cu'],
             extra_compile_args={
                 'cxx': ['-O3', '-std=c++17'],
-                'nvcc': ['-O3', '-std=c++17',
-                         '-gencode=arch=compute_60,code=sm_60',
-                         '-gencode=arch=compute_60,code=compute_60',
+                'nvcc': ['-O3', '-std=c++17', '-allow-unsupported-compiler',
                          '-gencode=arch=compute_70,code=sm_70',
                          '-gencode=arch=compute_75,code=sm_75',
                          '-gencode=arch=compute_75,code=compute_75',
